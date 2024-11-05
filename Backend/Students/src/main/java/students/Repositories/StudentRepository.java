ae92a3db8e52cd8939cee83056405876a4e3c272
Arin Sudakijjathorn <28irin62@gmail.com>
2024-11-05 23:54:13 +0700

    ควย

--- Backend/Students/src/main/java/students/Repositories/StudentRepository.java
+++ Backend/Students/src/main/java/students/Repositories/StudentRepository.java
@@ -1,9 +1,8 @@
 package students.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 import students.Entity.Students;
 
-@Service
 public interface StudentRepository extends JpaRepository<Students, Integer> {
+
 }
